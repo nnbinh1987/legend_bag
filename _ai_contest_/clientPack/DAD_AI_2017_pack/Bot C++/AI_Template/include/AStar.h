116f8e56f7dde370af9b8824314d21a00057b32d
Nhan Ngoc Binh <nnbinh1987@outlook.com>
2017-06-02 16:27:28 +0700

    upd2

---
+++ _ai_contest_/clientPack/DAD_AI_2017_pack/Bot C++/AI_Template/include/AStar.h
@@ -0,0 +1,70 @@
+/*
+    Copyright (c) 2015, Damian Barczynski <daan.net@wp.eu>
+    Following tool is licensed under the terms and conditions of the ISC license.
+    For more information visit https://opensource.org/licenses/ISC.
+*/
+#ifndef __ASTAR_HPP_8F637DB91972F6C878D41D63F7E7214F__
+#define __ASTAR_HPP_8F637DB91972F6C878D41D63F7E7214F__
+
+#include <vector>
+#include <functional>
+#include <set>
+
+#include <ai/AI.h>
+
+#define Vec2i Position
+
+namespace AStar
+{
+
+
+    using uint = unsigned int;
+    using HeuristicFunction = std::function<uint(Vec2i, Vec2i)>;
+    using CoordinateList = std::vector<Vec2i>;
+
+    struct Node
+    {
+        uint G, H;
+        Vec2i coordinates;
+        Node *parent;
+
+        Node(Vec2i coord_, Node *parent_ = nullptr);
+        uint getScore();
+    };
+
+    using NodeSet = std::set<Node*>;
+
+    class Generator
+    {
+        bool detectCollision(Vec2i coordinates_);
+        Node* findNodeOnList(NodeSet& nodes_, Vec2i coordinates_);
+        void releaseNodes(NodeSet& nodes_);
+
+    public:
+        Generator();
+        void setHeuristic(HeuristicFunction heuristic_);
+        CoordinateList findPath(Vec2i source_, Vec2i target_);
+        void addCollision(Vec2i coordinates_);
+		void setCollision(CoordinateList& listCollision);
+        void removeCollision(Vec2i coordinates_);
+        void clearCollisions();
+        
+    private:
+        HeuristicFunction heuristic;
+        CoordinateList direction, walls;
+		Vec2i worldSize = { 11,11 };
+        uint directions = 4;
+    };
+
+    class Heuristic
+    {
+        static Vec2i getDelta(Vec2i source_, Vec2i target_);
+
+    public:
+        static uint manhattan(Vec2i source_, Vec2i target_);
+        static uint euclidean(Vec2i source_, Vec2i target_);
+        static uint octagonal(Vec2i source_, Vec2i target_);
+    };
+}
+
+#endif // __ASTAR_HPP_8F637DB91972F6C878D41D63F7E7214F__
